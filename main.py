b44249ab81dd1b440e778e992343abf163bb891a
Sai Suraj <84668491+saisurajch@users.noreply.github.com>
2022-12-16 13:22:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from art import logo
+
+bidding_log = {}
+
+
+satisfied = True
+print(logo)
+while (satisfied == True):
+  name = input("What is your name: ")
+  bid_amount = int(input("What is your bid amount: $"))
+  bidding_log[name] = bid_amount
+  bidders = input("Are there any other bidders? Type 'yes' or 'no'.")
+  if(bidders == 'yes'):
+    clear()
+    satisfied = True
+  else:
+    clear()
+    satisfied = False
+    max = 0
+    winner = ''
+    for bidder in bidding_log:
+      bid = bidding_log[bidder]
+      if(bid>max):
+          max = bid
+          winner = bidder
+    print(f"The winner is {winner} with a bid of {max}")
